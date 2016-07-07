c0d936e36d2dce748cefd4e274be116907f0eca8
Sasho Najdov <sase.najdov@yahoo.com>
2016-07-07 14:35:13 +0200

    Update User class - Add Enum MessageCategory

---
+++ src/main/java/com/endava/intern/model/MessageCategory.java
@@ -0,0 +1,10 @@
+package com.endava.intern.model;
+
+/**
+ * Created by snajdov on 7/7/16.
+ */
+public enum MessageCategory {
+    Start,
+    Stop,
+    Continue
+}
--- src/main/java/com/endava/intern/model/User.java
+++ src/main/java/com/endava/intern/model/User.java
@@ -1,7 +1,9 @@
 package com.endava.intern.model;
 
+import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by snajdov on 7/7/16.
@@ -10,72 +12,19 @@ public class User {
 
     private String name;
     private String sessionId;
-    private List<String> messagesStart;
-    private List<String> messagesStop;
-    private List<String> messagesContinue;
+    @OneToMany
+    List<Message> messages;
 
     public User() {
         super();
     }
 
-    public User(String name, String sessionId) {
-        this.name = name;
-        this.sessionId = sessionId;
-        messagesStart = new ArrayList<>();
-        messagesStop = new ArrayList<>();
-        messagesContinue = new ArrayList<>();
+    public void addMessage(Message message) {
+        messages.add(message);
     }
 
-    public void addMessageStart(String message) {
-        messagesStart.add(message);
+    public List<Message> getMessages() {
+        return messages;
     }
 
-    public void addMessageStop(String message) {
-        messagesStop.add(message);
-    }
-
-    public void addMessageContinue(String message) {
-        messagesContinue.add(message);
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSessionId() {
-        return sessionId;
-    }
-
-    public void setSessionId(String sessionId) {
-        this.sessionId = sessionId;
-    }
-
-    public List<String> getMessagesStart() {
-        return messagesStart;
-    }
-
-    public void setMessagesStart(List<String> messagesStart) {
-        this.messagesStart = messagesStart;
-    }
-
-    public List<String> getMessagesStop() {
-        return messagesStop;
-    }
-
-    public void setMessagesStop(List<String> messagesStop) {
-        this.messagesStop = messagesStop;
-    }
-
-    public List<String> getMessagesContinue() {
-        return messagesContinue;
-    }
-
-    public void setMessagesContinue(List<String> messagesContinue) {
-        this.messagesContinue = messagesContinue;
-    }
-    
 }
