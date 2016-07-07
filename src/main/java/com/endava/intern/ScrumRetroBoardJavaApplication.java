be3ace825ed963e2bbd30a888030166fcc6f04b2
Sasho Najdov <snajdov@EN300003.endava.net>
2016-07-07 13:06:22 +0200

    Initial commit

---
+++ src/main/java/com/endava/intern/ScrumRetroBoardJavaApplication.java
@@ -0,0 +1,12 @@
+package com.endava.intern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumRetroBoardJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrumRetroBoardJavaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/endava/intern/ScrumRetroBoardJavaApplicationTests.java
@@ -0,0 +1,18 @@
+package com.endava.intern;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ScrumRetroBoardJavaApplication.class)
+@WebAppConfiguration
+public class ScrumRetroBoardJavaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
