5a00791266aa2b6a1f85cc7e510791369bb707c4
snajdov <sase.najdov@yahoo.com>
2016-07-18 13:46:14 +0200

    Modify MessageRepository

--- src/main/java/com/endava/intern/repository/MessageRepository.java
+++ src/main/java/com/endava/intern/repository/MessageRepository.java
@@ -1,23 +1,14 @@
 package com.endava.intern.repository;
 
 import com.endava.intern.model.Message;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.CachePut;
 import org.springframework.data.repository.CrudRepository;
 
-import javax.persistence.Cacheable;
 import java.util.Set;
-
-import static javafx.scene.input.KeyCode.T;
 
 /**
  * Created by jarsov on 7/12/2016.
  */
 
 public interface MessageRepository extends CrudRepository<Message,Integer> {
-
     Set<Message> findByCategory(String category);
-
-
-
 }
